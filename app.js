e863cbec4086478f3bfc2ff777a8c8db9d422cb1
bhanu-pratap439 <bhanupratap78784@gmail.com>
2024-12-16 14:48:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function signIn() {
+    
+    alert("Redirecting to Sign In page...");
+   
+    window.location.href = "signin.html";
+}
+document.getElementById("signInBtn").addEventListener("click", function() {
+    alert("Redirecting to Sign In page...");
+    window.location.href = "signin.html"; 
+});
